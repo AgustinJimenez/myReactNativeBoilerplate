d0ce2ab55ee55c7b6f902ceb7254b77c0f126738
Agustin <agus.jimenez.caba@gmail.com>
2019-02-01 00:27:43 -0300

    expo removed

--- src/boot/AplicationSetup.js
+++ src/boot/AplicationSetup.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react"
 import { StyleProvider } from "native-base"
-import * as Expo from "expo"
 import getTheme from "../theme/components"
 import variables from "../theme/variables/commonColor"
 import NavigationContainer from "app/src/Router"
@@ -12,26 +11,29 @@ export default class AplicationSetup extends Component
     super()
     this.state = 
     {
-      isReady: false
+      appIsReady: false
     }
   }
   async componentWillMount() 
   {
+    /*
     await Expo.Font.loadAsync
     ({
       Roboto: require("native-base/Fonts/Roboto.ttf"),
       Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
       Ionicons: require("native-base/Fonts/Ionicons.ttf")
     })
+    */
 
-    this.setState({ isReady: true })
+    this.setState({ appIsReady: true })
   }
 
   render() 
   {
-    if (!this.state.isReady) 
+/*
+    if (!this.state.appIsReady) 
       return <Expo.AppLoading />
-
+*/
     return (
       <StyleProvider 
       style={getTheme(variables)}
